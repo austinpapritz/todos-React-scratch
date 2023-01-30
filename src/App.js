8040149937f87902a93a46780a23a61d7971849e
Austin Papritz <austin@papritz.dev>
2023-01-30 13:45:17 -0800

    created routes to auth, wrapped App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth.js';
 
 function App() {
+  const { user } = useUser();
   return (
     <div className="App">
-      <Auth />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route exact path="/">
+          <>
+            {user && <Redirect to="/items" />}
+            {!user && <Redirect to="/auth/sign-in" />}
+          </>
+        </Route>
+      </Switch>
     </div>
   );
 }
