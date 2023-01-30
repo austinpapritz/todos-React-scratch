80bc117e678d1a1ddf6fc2343de6a191c0ada86d
Austin Papritz <austin@papritz.dev>
2023-01-30 12:50:27 -0800

    made component Todo and context folders, copied Authfrom demo, cleaned up App.js

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,92 @@
+import { NavLink, Redirect, useParams } from 'react-router-dom';
+
+import './Auth.css';
+import { useState } from 'react';
+import { authUser } from '../../services/auth';
+import { useUser } from '../../context/UserContext';
+
+export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { type } = useParams();
+  const { user, setUser } = useUser();
+  if (user) {
+    return <Redirect to="/items" />;
+  }
+  const submitAuth = async () => {
+    // TODO
+    // call authUser with email, password, type
+    try {
+      // if successful
+      const newUser = await authUser(email, password, type);
+      // setUser in context
+      setUser(newUser);
+      // redirect to items -- happens through a re-render
+      // which triggers line 15 Redirect
+    } catch (e) {
+      // else display error
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="auth box">
+      <nav className="panel is-success">
+        <div className="panel-heading">Welcome to Alchemy Shopping List</div>
+        <div className="panel-tabs">
+          <NavLink
+            className="is-size-6 has-text-weight-bold"
+            to="/auth/sign-in"
+            activeClassName="is-active"
+          >
+            Sign In
+          </NavLink>
+          <NavLink
+            className="is-size-6 has-text-weight-bold"
+            to="/auth/sign-up"
+            activeClassName="is-active"
+          >
+            Sign Up
+          </NavLink>
+        </div>
+        <div className="panel-block">
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="email"
+                placeholder="name@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="icon is-small is-left">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </span>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="icon is-small is-left">
+                <FontAwesomeIcon icon={faLock} />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="control">
+          <button onClick={submitAuth} className="button is-success mt-2 mb-2">
+            Submit
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
