0975419a6fc1e50955aa4dfb69bc30af027eadd5
Austin Papritz <austin@papritz.dev>
2023-01-30 16:03:50 -0800

    added header which checks if active user is logged in and shows either Welcome and signout button if true or redirects to auth if false

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Auth from './components/Auth/Auth.js';
 import { useUser } from './context/UserContext.js';
 
 import Todo from './components/Todo/Todo.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   const { user } = useUser();
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/list" component={Todo} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
+import { signOut } from '../../services/auth.js';
+
+export default function Header() {
+  const [isActive, setIsActive] = useState(false);
+  const { user, setUser } = useUser();
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      setUser(null);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  return (
+    <nav role="navigation">
+      <div className="nav-container">
+        <h1>✔️ Todo List ✔️ </h1>
+        <a
+          className={`navbar-burger ${isActive ? 'is-active' : ''}`}
+          onClick={() => setIsActive((prev) => !prev)}
+        ></a>
+      </div>
+      <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id="navbar-list">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            {!user && (
+              <div className="buttons">
+                <Link className="button" to="/auth/sign-up">
+                  <strong>Sign up</strong>
+                </Link>
+                <Link className="button" to="/auth/sign-in">
+                  <strong>Sign in</strong>
+                </Link>
+              </div>
+            )}
+            {user && (
+              <>
+                <div>Welcome, {user.email}!</div>
+                <button className="button" onClick={handleLogout}>
+                  Sign Out
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      {}
+    </nav>
+  );
+}
